fa593ebc60a7ebabda4dc23a0da9d0a1be998e25
Lim <40200760+tbnsok40@users.noreply.github.com>
2020-06-22 15:49:02 +0900

    Add files via upload

---
+++ Basics/challenge5.js
@@ -0,0 +1,33 @@
+///// function /////
+function tipCal(original) {
+    var percentage;
+    if (original < 50) {
+        percentage = .2;
+    } else if (original >= 50 && original < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * original
+}
+
+///// Variables /////
+
+var john = new Object()
+john['bill'] = new Array(124, 48, 268, 180, 42)
+
+var tip = new Array()
+john['tip'] = tip
+
+var final = new Array()
+
+console.log(john.bill[0])
+
+for (i = 0; i < john.bill.length; i++) {
+    tip[i] = tipCal(john.bill[i])
+    final[i] = tip[i] + john.bill[i]
+}
+john['final'] = final
+
+
+console.log('tips are ' + john.tip + ' and fianl paid is ' + john.final)
